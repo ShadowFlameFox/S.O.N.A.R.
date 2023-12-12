f1e015593411e4c415771ffbef14c10b7e6b6294
ShadowFlameFox <114284784+ShadowFlameFox@users.noreply.github.com>
2023-12-12 20:25:52 +0100

    Update api.py

--- S.O.N.A.R/api.py
+++ S.O.N.A.R/api.py
@@ -1,32 +1,49 @@
 from flask import Flask, jsonify
 import index
+import configparser
+
+def read_config(file_path):
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    return config
 
 app = Flask(__name__)
 
 @app.route('/create/<database_name>')
 def create_route(database_name):
-    result_message = index.create_database(database_name)
-    return jsonify({'result': result_message})
+    message, content ,status_code = index.create_database(database_name)
+    return jsonify({'message': message, 'value':content, 'status_code': status_code})
 
 @app.route('/store/<database_name>/<key>/<value>')
 def store_route(database_name, key, value):
-    result_message = index.store(database_name, key, value)
-    return jsonify({'result': result_message})
+    message, content, status_code = index.store(database_name, key, value)
+    return jsonify({'message': message, 'value':content, 'status_code': status_code})
 
 @app.route('/get/<database_name>/<key>')
 def load_route(database_name, key):
-    result_message = index.load(database_name, key)
-    return jsonify({'result': result_message})
+    message, content, status_code = index.load(database_name, key)
+    return jsonify({'message': message, 'value':content, 'status_code': status_code})
 
 @app.route('/list')
 def list_route():
-    result_message = index.list_subfolders()
-    return jsonify({'result': result_message})
+    message, content, status_code = index.list_subfolders()
+    return jsonify({'message': message, 'value':content, 'status_code': status_code})
 
 @app.route('/delete_key/<database_name>/<key>')
 def delete_key_route(database_name, key):
-    result_message = index.delete_key(database_name, key)
-    return jsonify({'result': result_message})
+    message, content, status_code = index.delete_key(database_name, key)
+    return jsonify({'message': message, 'value':content, 'status_code': status_code})
+
+@app.route('/delete_database/<database_name>')
+def delete_database_route(database_name):
+    message, content, status_code = index.delete_database(database_name)
+    return jsonify({'message': message, 'value':content, 'status_code': status_code})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    config_file_path = 'sonar.conf'
+    config = read_config(config_file_path)
+
+    host = config.get('API', 'host')
+    port = config.getint('API', 'port')
+
+    app.run(debug=True, port=port, host=host)

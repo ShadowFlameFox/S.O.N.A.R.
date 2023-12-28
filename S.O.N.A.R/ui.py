17db92d1ef4730fe1106d34329a8278479bcb180
ShadowFlameFox <114284784+ShadowFlameFox@users.noreply.github.com>
2023-12-28 17:43:34 +0100

    Update ui.py

--- S.O.N.A.R/ui.py
+++ S.O.N.A.R/ui.py
@@ -5,26 +5,32 @@ def exc(command):
         exit()
     elif "CREATE " in command:
         name = str(command).replace("CREATE ", "")
-        print(index.create_database(name))
+        msg, value, code = index.create_database(name)
     elif "GET" in command:
         name = str(command).replace("GET ", "")
         attributes = name.split(" ")
-        print(index.load(attributes[0], attributes[1]))
+        msg, value, code = index.load(attributes[0], attributes[1])
     elif "STORE" in command:
         name = str(command).replace("STORE ", "")
         attributes = name.split(" ")
-        print(index.store(attributes[0],attributes[1],attributes[2]))
+        msg, value, code = index.store(attributes[0],attributes[1],attributes[2])
     elif "LIST" in command:
         name = str(command).replace("LIST ", "")
-        print(index.list_subfolders())
+        msg, value, code = index.list_subfolders()
     elif "DELETE_KEY" in command:
         name = str(command).replace("DELETE_KEY ", "")
         attributes = name.split(" ")
-        print(index.delete_key(attributes[0], attributes[1]))
+        msg, value, code = index.delete_key(attributes[0], attributes[1])
     elif "DELETE_DATABASE" in command:
         name = str(command).replace("DELETE_DATABASE ", "")
-        print(index.delete_database(name))
+        msg, value, code = index.delete_database(name)
+    else:
+        return "Command not found!"
+    if value:
+        return value
+    else:
+        return msg
 
 if __name__ == "__main__":
     while True:
-        exc(input(": "))
+        print(exc(input(": ")))
